47c4c92fb58f29291b7a7e370b4a4083e3d13358
terry <grantkobe.bison@gmail.com>
2018-02-04 00:27:54 +0800

    finish jpg

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_curDir = new System.Windows.Forms.TextBox();
+            this.textBox_ouput = new System.Windows.Forms.TextBox();
+            this.text_Error_file = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(609, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始歸類";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "當前目錄";
+            // 
+            // textBox_curDir
+            // 
+            this.textBox_curDir.Location = new System.Drawing.Point(113, 34);
+            this.textBox_curDir.Name = "textBox_curDir";
+            this.textBox_curDir.Size = new System.Drawing.Size(540, 22);
+            this.textBox_curDir.TabIndex = 3;
+            // 
+            // textBox_ouput
+            // 
+            this.textBox_ouput.Location = new System.Drawing.Point(39, 470);
+            this.textBox_ouput.Name = "textBox_ouput";
+            this.textBox_ouput.Size = new System.Drawing.Size(573, 22);
+            this.textBox_ouput.TabIndex = 4;
+            // 
+            // text_Error_file
+            // 
+            this.text_Error_file.Location = new System.Drawing.Point(65, 169);
+            this.text_Error_file.Multiline = true;
+            this.text_Error_file.Name = "text_Error_file";
+            this.text_Error_file.Size = new System.Drawing.Size(453, 241);
+            this.text_Error_file.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 543);
+            this.Controls.Add(this.text_Error_file);
+            this.Controls.Add(this.textBox_ouput);
+            this.Controls.Add(this.textBox_curDir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_curDir;
+        private System.Windows.Forms.TextBox textBox_ouput;
+        private System.Windows.Forms.TextBox text_Error_file;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            /*
+            // Create an Image object. 
+            Image image = new Bitmap(@"d:\FakePhoto.jpg");
+
+            // Get the PropertyItems property from image.
+            PropertyItem[] propItems = image.PropertyItems;
+
+            // Set up the display.
+            Font font = new Font("Arial", 12);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            int X = 0;
+            int Y = 0;
+
+            // For each PropertyItem in the array, display the ID, type, and 
+            // length.
+            int count = 0;
+            string csContent = "" ;
+            foreach (PropertyItem propItem in propItems)
+            {
+                switch(propItem.Type)
+                {
+                    case 2:
+                        {
+                            csContent = Convert.ToString( propItem.Value);
+                            csContent = System.Text.Encoding.Default.GetString(propItem.Value);
+                            break;
+                        }
+                }
+                e.Graphics.DrawString(
+                "Property Item " + count.ToString(),
+                font,
+                blackBrush,
+                X, Y);
+
+                Y += font.Height;
+                  
+                e.Graphics.DrawString(
+                   "   iD: 0x" + propItem.Id.ToString("x"),
+                   font,
+                   blackBrush,
+                   X, Y);
+
+                Y += font.Height;
+
+                e.Graphics.DrawString(
+                   "   type: " + propItem.Type.ToString(),
+                   font,
+                   blackBrush,
+                   X, Y);
+
+                Y += font.Height;
+
+                e.Graphics.DrawString(
+                   "   length: " + propItem.Len.ToString() + " bytes" + "(" + csContent +")",
+                   font,
+                   blackBrush,
+                   X, Y);
+
+                Y += font.Height;
+
+                count++;
+            }
+            // Convert the value of the second property to a string, and display 
+            // it.
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            string manufacturer = encoding.GetString(propItems[1].Value);
+
+            e.Graphics.DrawString(
+               "The equipment make is " + manufacturer + ".",
+               font,
+               blackBrush,
+               X, Y);
+               */
+        }
+
+        int m_jpegFiles;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_curDir.Text = System.Environment.CurrentDirectory;
+          
+            List<string> dirs;
+            String[] FileCollection;
+            //
+            {
+
+                try
+                {
+                    //  dirs
+                    {
+                        string dirPath = textBox_curDir.Text;
+
+                        dirs = new List<string>(Directory.EnumerateDirectories(dirPath));
+
+                        foreach (var dir in dirs)
+                        {
+
+                            Console.WriteLine("{0}", dir.Substring(dir.LastIndexOf("\\") + 1));
+                        }
+
+                        Console.WriteLine("{0} directories found.", dirs.Count);
+                    }
+
+                    //files
+                    {
+                        String FilePath = textBox_curDir.Text;
+                        FileInfo theFileInfo;
+                        FileCollection = Directory.GetFiles(FilePath);
+
+                        for (int i = 0; i < FileCollection.Length; i++)
+                        {
+
+                            theFileInfo = new FileInfo(FileCollection[i]);
+                            if(theFileInfo.Extension.ToLower() == ".jpg")
+                            {
+                                m_jpegFiles++;
+                            }
+                            //Response.Write(theFileInfo.Name.ToString() + "<BR>");
+                        }
+                    }
+
+                    textBox_ouput.Text = "當前目錄:" + dirs.Count.ToString() + " 檔案:" + FileCollection.Length.ToString() +"jpg :" + m_jpegFiles;
+                }
+                catch (UnauthorizedAccessException UAEx)
+                {
+                    Console.WriteLine(UAEx.Message);
+                }
+                catch (PathTooLongException PathEx)
+                {
+                    Console.WriteLine(PathEx.Message);
+                }
+
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            List<string> dirs;
+            String[] FileCollection;
+            //
+            {
+
+                try
+                {
+                    //find how many jpeg files
+                    {
+                        m_jpegFiles = 0;
+                        //files
+                        {
+                            String FilePath = textBox_curDir.Text;
+                            FileInfo theFileInfo;
+                            FileCollection = Directory.GetFiles(FilePath);
+
+                            for (int i = 0; i < FileCollection.Length; i++)
+                            {
+
+                                theFileInfo = new FileInfo(FileCollection[i]);
+                                if (theFileInfo.Extension.ToLower() == ".jpg")
+                                {
+                                    m_jpegFiles++;
+                                }
+                                //Response.Write(theFileInfo.Name.ToString() + "<BR>");
+                            }
+                        }
+
+                    }
+
+                    int left_files = m_jpegFiles;
+                    //files
+                    {
+                        String FilePath = textBox_curDir.Text;
+                        FileInfo theFileInfo;
+                        FileCollection = Directory.GetFiles(FilePath);
+
+                        for (int i = 0; i < FileCollection.Length; i++)
+                        {
+                            string Year = "";
+                            string Day_hour = "" ;
+                            string Moth = "";
+                            string Day = "";
+
+                            theFileInfo = new FileInfo(FileCollection[i]);
+                            if(theFileInfo.Extension.ToLower() == ".jpg")
+                            {
+                              
+
+                                // Create an Image object. 
+                                Image image = new Bitmap(theFileInfo.FullName);
+
+                                // Get the PropertyItems property from image.
+                                PropertyItem[] propItems = image.PropertyItems;
+                             
+                               
+
+                                // For each PropertyItem in the array, display the ID, type, and 
+                                // length.
+                                int count = 0;
+                                string csPhtoTime = "";
+                                bool bIfFoundPhotoTime = false;
+                               
+                              
+                                foreach (PropertyItem propItem in propItems)
+                                {
+                                    switch (propItem.Id)
+                                    {
+                                        case 0x9003://or 9004
+                                            {
+                                                //csPhtoTime = Convert.ToString(propItem.Value);
+                                                csPhtoTime = System.Text.Encoding.Default.GetString(propItem.Value);
+                                                string[] tokens = csPhtoTime.Split(':');
+                                                Year = tokens[0];
+                                                Moth = tokens[1];
+
+                                                Day_hour = tokens[2];
+                                                
+                                                tokens = Day_hour.Split(' ');
+                                                Day = tokens[0];
+                                                bIfFoundPhotoTime = true;
+                                                break;
+                                            }
+                                    }
+                                }
+
+                                if (false ==  bIfFoundPhotoTime )
+                                {
+                                    return;
+                                }
+                                else {
+                                    string copyDir_fullPath = "";
+                                    string Year_fullPath = textBox_curDir.Text + "\\" + Year;
+                                    if (!System.IO.Directory.Exists(Year_fullPath))
+                                    {
+                                        System.IO.Directory.CreateDirectory(Year_fullPath);
+                                    }
+
+                                    //rename Month to chinese 
+                                    {
+                                       var MonthMap = new Dictionary<string, string>();
+                                        MonthMap.Add("01", "一月");
+                                        MonthMap.Add("02", "二月");
+                                        MonthMap.Add("03", "三月");
+                                        MonthMap.Add("04", "四月");
+                                        MonthMap.Add("05", "五月");
+                                        MonthMap.Add("06", "六月");
+                                        MonthMap.Add("07", "七月");
+                                        MonthMap.Add("08", "八月");
+                                        MonthMap.Add("09", "九月");
+                                        MonthMap.Add("10", "十月");
+                                        MonthMap.Add("11", "十一月");
+                                        MonthMap.Add("12", "十二月");
+                                        
+                                         MonthMap.TryGetValue(Moth, out Moth);
+
+                                    }
+                                    string Month_fullPath = Year_fullPath + "\\" + Moth;
+                                    if (!System.IO.Directory.Exists(Month_fullPath))
+                                    {
+                                        System.IO.Directory.CreateDirectory(Month_fullPath);
+                                    }
+                                    copyDir_fullPath = Month_fullPath + "\\"+ theFileInfo.Name;
+                                    //move to the directory
+                                    try
+                                    {
+                                     
+                                        image = null;
+                                        if (false == System.IO.File.Exists(copyDir_fullPath))
+                                        {
+                                            System.IO.File.Copy(theFileInfo.FullName, copyDir_fullPath);
+                                        }
+                                    }catch(Exception j)
+                                    {
+                                        text_Error_file.Text += theFileInfo.Name +":"+ j.Message + "\n";
+                                        int g1 = 0;
+                                        g1++;
+                                    }
+                                    left_files--;
+                                    textBox_ouput.Text = "剩下檔案:" + left_files;
+
+                                }
+
+                                    
+                                int g = 0;
+                                g++;
+
+                            }
+                            //Response.Write(theFileInfo.Name.ToString() + "<BR>");
+                        }
+                    }
+
+                  
+                }
+                catch (UnauthorizedAccessException UAEx)
+                {
+                    Console.WriteLine(UAEx.Message);
+                }
+                catch (PathTooLongException PathEx)
+                {
+                    Console.WriteLine(PathEx.Message);
+                }
+
+            }
+
+
+        }
+    }
+}

0992800784c4ec77755ce464d1dbc39cfa2ea42f
terry <grantkobe.bison@gmail.com>
2018-03-05 02:56:40 +0800

    ttt

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -105,8 +105,30 @@ namespace WindowsFormsApp1
                X, Y);
                */
         }
+        int ijpeg_fileNum = 0;
+        int imov_filenum = 0;
+        int ipng_filenum = 0;
+      
 
-        int m_jpegFiles;
+        private int FindFiles(out String[] FileCollection, String FilePath, string search_type)
+        {
+            int fileNum = 0;
+            FileInfo theFileInfo;
+            FileCollection = Directory.GetFiles(FilePath);
+
+            for (int i = 0; i < FileCollection.Length; i++)
+            {
+
+                theFileInfo = new FileInfo(FileCollection[i]);
+                if (theFileInfo.Extension.ToLower() == search_type.ToLower())
+                {
+                    fileNum++;
+                }
+                //Response.Write(theFileInfo.Name.ToString() + "<BR>");  123
+            }
+            return fileNum;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox_curDir.Text = System.Environment.CurrentDirectory;
@@ -124,6 +146,13 @@ namespace WindowsFormsApp1
 
                         dirs = new List<string>(Directory.EnumerateDirectories(dirPath));
 
+
+
+
+
+
+
+
                         foreach (var dir in dirs)
                         {
 
@@ -135,24 +164,20 @@ namespace WindowsFormsApp1
 
                     //files
                     string search_type = ".jpg";
-                    {
-                        String FilePath = textBox_curDir.Text;
-                        FileInfo theFileInfo;
-                        FileCollection = Directory.GetFiles(FilePath);
-                       
-                        for (int i = 0; i < FileCollection.Length; i++)
-                        {
+                    String FilePath = textBox_curDir.Text;
+                    int ijpeg_fileNum = 0;
+                    ijpeg_fileNum = FindFiles(out FileCollection, FilePath, search_type);
 
-                            theFileInfo = new FileInfo(FileCollection[i]);
-                            if(theFileInfo.Extension.ToLower() == search_type)
-                            {
-                                m_jpegFiles++;
-                            }
-                            //Response.Write(theFileInfo.Name.ToString() + "<BR>");  123
-                        }
-                    }
+                    int imov_filenum = 0;
+                    imov_filenum = FindFiles(out FileCollection, FilePath, ".mov");
 
-                    textBox_ouput.Text = "當前目錄:" + dirs.Count.ToString() + " 檔案:" + FileCollection.Length.ToString() +"jpg :" + m_jpegFiles;
+                    int ipng_filenum = 0;
+                    ipng_filenum = FindFiles(out FileCollection, FilePath, ".png");
+
+
+
+
+                    textBox_ouput.Text = "當前目錄:" + dirs.Count.ToString() + " 檔案:" + FileCollection.Length.ToString() +"jpg :" + ijpeg_fileNum + "mov:" + imov_filenum + "png:" + ipng_filenum;
                 }
                 catch (UnauthorizedAccessException UAEx)
                 {
@@ -169,6 +194,53 @@ namespace WindowsFormsApp1
 
         }
 
+        private int MoveByModifiedTime(FileInfo theFileInfo, ref Dictionary<string, string> MonthMap, ref int left_files)
+        {
+
+
+            var LastWriteTime = theFileInfo.LastAccessTimeUtc;
+            string copyDir_fullPath;
+            string Moth;
+
+            string Year_fullPath = textBox_curDir.Text + "\\" + LastWriteTime.Year;
+            if (!System.IO.Directory.Exists(Year_fullPath))
+            {
+                System.IO.Directory.CreateDirectory(Year_fullPath);
+            }
+
+            //rename Month to chinese 
+            {
+
+
+                MonthMap.TryGetValue(LastWriteTime.Month.ToString(), out Moth);
+
+            }
+            string Month_fullPath = Year_fullPath + "\\" + Moth;
+            if (!System.IO.Directory.Exists(Month_fullPath))
+            {
+                System.IO.Directory.CreateDirectory(Month_fullPath);
+            }
+            copyDir_fullPath = Month_fullPath + "\\" + theFileInfo.Name;
+            //move to the directory
+            try
+            {
+
+
+                if (false == System.IO.File.Exists(copyDir_fullPath))
+                {
+                    System.IO.File.Copy(theFileInfo.FullName, copyDir_fullPath);
+                }
+            }
+            catch (Exception j)
+            {
+                text_Error_file.Text += theFileInfo.Name + ":" + j.Message + "\n";
+                int g1 = 0;
+                g1++;
+            }
+            left_files--;
+            textBox_ouput.Text = "剩下檔案:" + left_files; return left_files;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
 
@@ -179,36 +251,64 @@ namespace WindowsFormsApp1
 
                 try
                 {
-                    //find how many jpeg files 
-                    {
-                        m_jpegFiles = 0;
-                        //files
-                        {
-                            String FilePath = textBox_curDir.Text;
-                            FileInfo theFileInfo;
-                            FileCollection = Directory.GetFiles(FilePath);
+                    var MonthMap = new Dictionary<string, string>();
+                    MonthMap.Add("01", "一月");
+                    MonthMap.Add("1", "一月");
+                    MonthMap.Add("02", "二月");
+                    MonthMap.Add("2", "二月");
+                    MonthMap.Add("03", "三月");
+                    MonthMap.Add("3", "三月");
+                    MonthMap.Add("04", "四月");
+                    MonthMap.Add("4", "四月");
+                    MonthMap.Add("05", "五月");
+                    MonthMap.Add("5", "五月");
+                    MonthMap.Add("06", "六月");
+                    MonthMap.Add("6", "六月");
+                    MonthMap.Add("07", "七月");
+                    MonthMap.Add("7", "七月");
+                    MonthMap.Add("08", "八月");
+                    MonthMap.Add("8", "八月");
+                    MonthMap.Add("09", "九月");
+                    MonthMap.Add("9", "九月");
+                    MonthMap.Add("10", "十月");
+                    MonthMap.Add("11", "十一月");
+                    MonthMap.Add("12", "十二月");
 
-                            for (int i = 0; i < FileCollection.Length; i++)
-                            {
-
-                                theFileInfo = new FileInfo(FileCollection[i]);
-                                if (theFileInfo.Extension.ToLower() == ".jpg")
-                                {
-                                    m_jpegFiles++;
-                                }
-                                //Response.Write(theFileInfo.Name.ToString() + "<BR>");
-                            }
-                        }
-
-                    }
-
-                    int left_files = m_jpegFiles;
+                    int left_files = ijpeg_fileNum + imov_filenum + ipng_filenum;
                     //files
                     {
                         String FilePath = textBox_curDir.Text;
                         FileInfo theFileInfo;
                         FileCollection = Directory.GetFiles(FilePath);
 
+
+                        //handle mov && png, which choose by modified time
+                        {
+                            for (int i = 0; i < FileCollection.Length; i++)
+                            {
+                                bool bProcessFile = false;
+
+                                theFileInfo = new FileInfo(FileCollection[i]);
+                                if (theFileInfo.Extension.ToLower() == ".mov".ToLower())
+                                {
+                                    bProcessFile = true;
+                                } else if (theFileInfo.Extension.ToLower() == ".png".ToLower())
+                                {
+                                    bProcessFile = true;
+                                }
+
+                                if (false == bProcessFile) continue;
+
+                                {
+                                     MoveByModifiedTime(theFileInfo, ref MonthMap, left_files);
+
+                                }
+
+
+
+                            }
+                        }
+                        //handle jpeg, choosed by picture time
                         for (int i = 0; i < FileCollection.Length; i++)
                         {
                             string Year = "";
@@ -231,7 +331,7 @@ namespace WindowsFormsApp1
 
                                 // For each PropertyItem in the array, display the ID, type, and 
                                 // length.
-                                int count = 0;
+                              
                                 string csPhtoTime = "";
                                 bool bIfFoundPhotoTime = false;
                                
@@ -258,6 +358,7 @@ namespace WindowsFormsApp1
                                     }
                                 }
 
+                                //choose by modified time
                                 if (false ==  bIfFoundPhotoTime )
                                 {
                                     return;
@@ -272,19 +373,7 @@ namespace WindowsFormsApp1
 
                                     //rename Month to chinese 
                                     {
-                                       var MonthMap = new Dictionary<string, string>();
-                                        MonthMap.Add("01", "一月");
-                                        MonthMap.Add("02", "二月");
-                                        MonthMap.Add("03", "三月");
-                                        MonthMap.Add("04", "四月");
-                                        MonthMap.Add("05", "五月");
-                                        MonthMap.Add("06", "六月");
-                                        MonthMap.Add("07", "七月");
-                                        MonthMap.Add("08", "八月");
-                                        MonthMap.Add("09", "九月");
-                                        MonthMap.Add("10", "十月");
-                                        MonthMap.Add("11", "十一月");
-                                        MonthMap.Add("12", "十二月");
+                                 
                                         
                                          MonthMap.TryGetValue(Moth, out Moth);
 

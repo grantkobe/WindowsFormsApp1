14ba644855c7e0f3bbc7a34bb2f4ad1564f99e9d
Terry wu <terry_wu@bisontech.com.tw>
2018-04-03 14:52:33 +0800

    add sort by day

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,13 +33,18 @@
             this.textBox_curDir = new System.Windows.Forms.TextBox();
             this.textBox_ouput = new System.Windows.Forms.TextBox();
             this.text_Error_file = new System.Windows.Forms.TextBox();
+            this.radioButton_month = new System.Windows.Forms.RadioButton();
+            this.radioButton_day = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(609, 126);
+            this.button1.Location = new System.Drawing.Point(44, 66);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "開始歸類";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,48 +53,88 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 37);
+            this.label1.Location = new System.Drawing.Point(49, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.Size = new System.Drawing.Size(67, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "當前目錄";
             // 
             // textBox_curDir
             // 
-            this.textBox_curDir.Location = new System.Drawing.Point(113, 34);
+            this.textBox_curDir.Location = new System.Drawing.Point(151, 42);
+            this.textBox_curDir.Margin = new System.Windows.Forms.Padding(4);
             this.textBox_curDir.Name = "textBox_curDir";
-            this.textBox_curDir.Size = new System.Drawing.Size(540, 22);
+            this.textBox_curDir.Size = new System.Drawing.Size(719, 25);
             this.textBox_curDir.TabIndex = 3;
             // 
             // textBox_ouput
             // 
-            this.textBox_ouput.Location = new System.Drawing.Point(39, 470);
+            this.textBox_ouput.Location = new System.Drawing.Point(52, 588);
+            this.textBox_ouput.Margin = new System.Windows.Forms.Padding(4);
             this.textBox_ouput.Name = "textBox_ouput";
-            this.textBox_ouput.Size = new System.Drawing.Size(573, 22);
+            this.textBox_ouput.Size = new System.Drawing.Size(763, 25);
             this.textBox_ouput.TabIndex = 4;
             // 
             // text_Error_file
             // 
-            this.text_Error_file.Location = new System.Drawing.Point(65, 169);
+            this.text_Error_file.Location = new System.Drawing.Point(87, 211);
+            this.text_Error_file.Margin = new System.Windows.Forms.Padding(4);
             this.text_Error_file.Multiline = true;
             this.text_Error_file.Name = "text_Error_file";
-            this.text_Error_file.Size = new System.Drawing.Size(453, 241);
+            this.text_Error_file.Size = new System.Drawing.Size(603, 300);
             this.text_Error_file.TabIndex = 5;
+            // 
+            // radioButton_month
+            // 
+            this.radioButton_month.AutoSize = true;
+            this.radioButton_month.Location = new System.Drawing.Point(44, 22);
+            this.radioButton_month.Name = "radioButton_month";
+            this.radioButton_month.Size = new System.Drawing.Size(73, 19);
+            this.radioButton_month.TabIndex = 6;
+            this.radioButton_month.Text = "按月份";
+            this.radioButton_month.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_day
+            // 
+            this.radioButton_day.AutoSize = true;
+            this.radioButton_day.Checked = true;
+            this.radioButton_day.Location = new System.Drawing.Point(123, 22);
+            this.radioButton_day.Name = "radioButton_day";
+            this.radioButton_day.Size = new System.Drawing.Size(73, 19);
+            this.radioButton_day.TabIndex = 7;
+            this.radioButton_day.TabStop = true;
+            this.radioButton_day.Text = "按日期";
+            this.radioButton_day.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton_day);
+            this.groupBox1.Controls.Add(this.radioButton_month);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(742, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(210, 112);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 543);
+            this.ClientSize = new System.Drawing.Size(1049, 679);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.text_Error_file);
             this.Controls.Add(this.textBox_ouput);
             this.Controls.Add(this.textBox_curDir);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +147,9 @@
         private System.Windows.Forms.TextBox textBox_curDir;
         private System.Windows.Forms.TextBox textBox_ouput;
         private System.Windows.Forms.TextBox text_Error_file;
+        private System.Windows.Forms.RadioButton radioButton_month;
+        private System.Windows.Forms.RadioButton radioButton_day;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,8 +12,25 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+    enum enSortType { enSortByMonth, enSortByDay };
+
     public partial class Form1 : Form
     {
+       
+
+        enSortType m_enSortType;
+        void _ChooseSortType(out enSortType etype)
+        {
+            if (true == radioButton_month.Checked)
+            {
+                etype = enSortType.enSortByMonth;
+            } else
+            {
+                etype= enSortType.enSortByDay;
+            }
+
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -194,11 +211,11 @@ namespace WindowsFormsApp1
 
         }
 
-        private int MoveByModifiedTime(FileInfo theFileInfo, ref Dictionary<string, string> MonthMap, ref int left_files)
+        private int MoveByModifiedTime(FileInfo theFileInfo, ref Dictionary<string, string> MonthMap, ref Dictionary<string, string> DayMap, ref int left_files)
         {
 
 
-            var LastWriteTime = theFileInfo.LastAccessTimeUtc;
+            var LastWriteTime = theFileInfo.LastWriteTimeUtc;
             string copyDir_fullPath;
             string Moth;
 
@@ -216,11 +233,34 @@ namespace WindowsFormsApp1
 
             }
             string Month_fullPath = Year_fullPath + "\\" + Moth;
+
+
             if (!System.IO.Directory.Exists(Month_fullPath))
             {
                 System.IO.Directory.CreateDirectory(Month_fullPath);
             }
-            copyDir_fullPath = Month_fullPath + "\\" + theFileInfo.Name;
+
+
+            if (enSortType.enSortByDay == m_enSortType)//add another sub dir
+            {
+                string strDay = ""+ LastWriteTime.Day;
+                {
+                    DayMap.TryGetValue(strDay, out strDay);
+                }
+                string Day_fullPath = Month_fullPath += "\\" + strDay;
+
+                if (!System.IO.Directory.Exists(Day_fullPath))
+                {
+                    System.IO.Directory.CreateDirectory(Day_fullPath);
+                }
+                copyDir_fullPath = Day_fullPath + "\\" + theFileInfo.Name;
+            }
+            else
+            {
+                copyDir_fullPath = Month_fullPath + "\\" + theFileInfo.Name;
+            }
+
+            
             //move to the directory
             try
             {
@@ -243,15 +283,59 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-            List<string> dirs;
+            _ChooseSortType(out m_enSortType);
+           // List<string> dirs;
             String[] FileCollection;
+
             //
             {
 
                 try
                 {
                     var MonthMap = new Dictionary<string, string>();
+                    var DayMap = new Dictionary<string, string>();
+
+                    DayMap.Add("01", "01");
+                    DayMap.Add("1", "01");
+                    DayMap.Add("02", "02");
+                    DayMap.Add("2", "02");
+                    DayMap.Add("03", "03");
+                    DayMap.Add("3", "03");
+                    DayMap.Add("04", "04");
+                    DayMap.Add("4", "04");
+                    DayMap.Add("05", "05");
+                    DayMap.Add("5", "05");
+                    DayMap.Add("06", "06");
+                    DayMap.Add("6", "06");
+                    DayMap.Add("07", "07");
+                    DayMap.Add("7", "07");
+                    DayMap.Add("08", "08");
+                    DayMap.Add("8", "08");
+                    DayMap.Add("09", "09");
+                    DayMap.Add("9", "09");
+                    DayMap.Add("10", "10");
+                    DayMap.Add("11", "11");
+                    DayMap.Add("12", "12");
+                    DayMap.Add("13", "13");
+                    DayMap.Add("14", "14");
+                    DayMap.Add("15", "15");
+                    DayMap.Add("16", "16");
+                    DayMap.Add("17", "17");
+                    DayMap.Add("18", "18");
+                    DayMap.Add("19", "19");
+                    DayMap.Add("20", "20");
+                    DayMap.Add("21", "21");
+                    DayMap.Add("22", "22");
+                    DayMap.Add("23", "23");
+                    DayMap.Add("24", "24");
+                    DayMap.Add("25", "25");
+                    DayMap.Add("26", "26");
+                    DayMap.Add("27", "27");
+                    DayMap.Add("28", "28");
+                    DayMap.Add("29", "29");
+                    DayMap.Add("30", "30");
+                    DayMap.Add("31", "31");
+
                     MonthMap.Add("01", "一月");
                     MonthMap.Add("1", "一月");
                     MonthMap.Add("02", "二月");
@@ -274,10 +358,16 @@ namespace WindowsFormsApp1
                     MonthMap.Add("11", "十一月");
                     MonthMap.Add("12", "十二月");
 
+                    string search_type = ".jpg";
+                    String FilePath = textBox_curDir.Text;
+                    ijpeg_fileNum = FindFiles(out FileCollection, FilePath, search_type);
+                    imov_filenum = FindFiles(out FileCollection, FilePath, ".mov");
+                    ipng_filenum = FindFiles(out FileCollection, FilePath, ".png");
+
                     int left_files = ijpeg_fileNum + imov_filenum + ipng_filenum;
                     //files
                     {
-                        String FilePath = textBox_curDir.Text;
+                        FilePath = textBox_curDir.Text;
                         FileInfo theFileInfo;
                         FileCollection = Directory.GetFiles(FilePath);
 
@@ -300,7 +390,7 @@ namespace WindowsFormsApp1
                                 if (false == bProcessFile) continue;
 
                                 {
-                                     MoveByModifiedTime(theFileInfo, ref MonthMap, ref left_files);
+                                     MoveByModifiedTime(theFileInfo, ref MonthMap, ref DayMap,ref left_files);
 
                                 }
 
@@ -320,9 +410,10 @@ namespace WindowsFormsApp1
                             if(theFileInfo.Extension.ToLower() == ".jpg")
                             {
                               
-
                                 // Create an Image object. 
                                 Image image = new Bitmap(theFileInfo.FullName);
+                               
+//                                System.IO.File.Move("C:\\Users\\terry\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\bin\\Debug\\FakePhoto.jpg", "C:\\Users\\terry\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\bin\\Debug\\2018\\二月\\FakePhoto.jpg");
 
                                 // Get the PropertyItems property from image.
                                 PropertyItem[] propItems = image.PropertyItems;
@@ -357,11 +448,13 @@ namespace WindowsFormsApp1
                                             }
                                     }
                                 }
-
+                                image.Dispose();
+                                propItems = null;
+                               
                                 //choose by modified time
                                 if (false ==  bIfFoundPhotoTime )
                                 {
-                                    MoveByModifiedTime(theFileInfo, ref MonthMap, ref left_files);
+                                    MoveByModifiedTime(theFileInfo, ref MonthMap, ref DayMap,ref left_files);
                                     continue;
                                 }
                                 else {
@@ -384,18 +477,37 @@ namespace WindowsFormsApp1
                                     {
                                         System.IO.Directory.CreateDirectory(Month_fullPath);
                                     }
-                                    copyDir_fullPath = Month_fullPath + "\\"+ theFileInfo.Name;
+
+                                    if (enSortType.enSortByDay == m_enSortType)//add another sub dir
+                                    {
+                                        string Day_fullPath = Month_fullPath += "\\" + Day;
+                                        {
+                                            DayMap.TryGetValue(Day, out Day);
+                                        }
+                                        if (!System.IO.Directory.Exists(Day_fullPath))
+                                        {
+                                            System.IO.Directory.CreateDirectory(Day_fullPath);
+                                        }
+                                        copyDir_fullPath = Day_fullPath + "\\" + theFileInfo.Name;
+                                    }
+                                    else
+                                    {
+                                        copyDir_fullPath = Month_fullPath + "\\" + theFileInfo.Name;
+                                    }
+
+                                    
                                     string src_path = theFileInfo.FullName;
                                     string src_name = theFileInfo.Name;
                                     //move to the directory
                                     try
                                     {
-                                     
-                                        image = null;
+                                        
+
+
                                         if (false == System.IO.File.Exists(copyDir_fullPath))
                                         {
-                                            
-                                            theFileInfo = null;
+                                           
+                                           // theFileInfo = null;
                                             System.IO.File.Move(src_path, copyDir_fullPath);
                                         }
                                     }catch(Exception j)
